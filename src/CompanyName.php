61790b0f18ff32e6d5d0cefe5b14f91a001c5f9b
pjordaan <pjordaan@users.noreply.github.com>
2024-03-24 22:01:25 +0000

    Update file(s) "packages/text-value-objects/." from "apie-lib/apie-lib-monorepo"

---
+++ src/CompanyName.php
@@ -0,0 +1,32 @@
+<?php
+namespace Apie\TextValueObjects;
+
+use Apie\Core\Attributes\FakeMethod;
+use Apie\Core\Attributes\ProvideIndex;
+use Apie\Core\ValueObjects\Interfaces\HasRegexValueObjectInterface;
+use Apie\Core\ValueObjects\IsStringWithRegexValueObject;
+use Apie\TextValueObjects\Concerns\IndexesWords;
+use Faker\Generator;
+
+#[FakeMethod('createRandom')]
+#[ProvideIndex('getIndexes')]
+class CompanyName implements HasRegexValueObjectInterface
+{
+    use IndexesWords;
+    use IsStringWithRegexValueObject;
+
+    public static function getRegularExpression(): string
+    {
+        return '/^\w[\w\-\s\'`"‘’“”‟,]*$/u';
+    }
+
+    protected function convert(string $input): string
+    {
+        return trim($input);
+    }
+
+    public static function createRandom(Generator $generator): self
+    {
+        return new static($generator->company());
+    }
+}
